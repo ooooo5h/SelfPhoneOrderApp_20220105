e715de2fbb4262538d1e8390f9f088044b98b56c
ooooo5h <ooooo5h91@gmail.com>
2022-01-05 17:58:24 +0900

    TedPermission, ActionCall 아직 다 이해못했음.. 하지만 전화걸기누르면 권한이 거절됐다는 토스트가 뜸

--- app/src/main/java/com/neppplus/selfphoneorderapp_20220105/ViewStoreDetailActivity.kt
+++ app/src/main/java/com/neppplus/selfphoneorderapp_20220105/ViewStoreDetailActivity.kt
@@ -1,10 +1,16 @@
 package com.neppplus.selfphoneorderapp_20220105
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.bumptech.glide.Glide
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.normal.TedPermission
 import com.neppplus.selfphoneorderapp_20220105.models.StoreData
 import kotlinx.android.synthetic.main.activity_view_store_detail.*
+import java.util.jar.Manifest
 
 class ViewStoreDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +22,28 @@ class ViewStoreDetailActivity : AppCompatActivity() {
         txtStoreName.text = storeData.Name
         txtPhoneNum.text = storeData.phoneNum
         Glide.with(this).load(storeData.logoUrl).into(imgLogo)
+
+        btnCall.setOnClickListener {
+
+            val pl = object : PermissionListener {
+                override fun onPermissionGranted() {
+
+                    val myUri = Uri.parse("tel:${storeData.phoneNum}")
+                    val myIntent = Intent(Intent.ACTION_CALL, myUri)
+                    startActivity(myIntent)
+                }
+
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+
+                    Toast.makeText(this@ViewStoreDetailActivity, "권한이 거절되었습니다.", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            TedPermission.create()
+                .setPermissionListener(pl)
+                .setPermissions(android.Manifest.permission.CALL_PHONE)
+                .check()
+
+        }
     }
 }
