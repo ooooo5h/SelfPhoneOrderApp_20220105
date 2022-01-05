3fc15c0416a55825fdae30c2954f3fe3766e85ba
ooooo5h <ooooo5h91@gmail.com>
2022-01-05 17:52:41 +0900

    가게 상세화면 모든 실제 데이터 구현 완료!

--- app/src/main/java/com/neppplus/selfphoneorderapp_20220105/ViewStoreDetailActivity.kt
+++ app/src/main/java/com/neppplus/selfphoneorderapp_20220105/ViewStoreDetailActivity.kt
@@ -2,10 +2,19 @@ package com.neppplus.selfphoneorderapp_20220105
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.neppplus.selfphoneorderapp_20220105.models.StoreData
+import kotlinx.android.synthetic.main.activity_view_store_detail.*
 
 class ViewStoreDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_store_detail)
+
+        val storeData = intent.getSerializableExtra("store") as StoreData
+
+        txtStoreName.text = storeData.Name
+        txtPhoneNum.text = storeData.phoneNum
+        Glide.with(this).load(storeData.logoUrl).into(imgLogo)
     }
 }
